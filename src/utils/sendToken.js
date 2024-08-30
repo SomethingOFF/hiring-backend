ea999ad8c75fd1f780a87bdd5e5165d47f130172
SomthingOFF <somethinoff@gmail.com>
2024-08-30 18:22:06 +0530

    cookies updated

--- src/utils/sendToken.js
+++ src/utils/sendToken.js
@@ -4,6 +4,9 @@ const sendToken = (user, statuscode, res, message) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
   };
   res.status(statuscode).cookie("auth", token, options).json({
     success: true,
